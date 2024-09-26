5f2200d360b967b2edf33ee647c512ad1bcece4a
Dino Fejzagić <dino.f@live.de>
2024-09-26 18:40:27 +0200

    v1.0.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,12 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.1.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [1.0.1]
+
+### Changed
+
+- Instead of using [InitializeOnLoad] for initiazing Android settings, we'll do it via -executeMethod
+
 ## [1.0.0]
 
 Initial release
--- Editor/InitializeAndroidExternalToolsSettings.cs
+++ Editor/InitializeAndroidExternalToolsSettings.cs
@@ -1,38 +1,32 @@
-using UnityEditor;
 using UnityEditor.Android;
 
 namespace Dinomite.AzurePipelines
 {
     public static class InitializeAndroidTools
     {
-        [InitializeOnLoadMethod]
-        public static void Initialze()
+        public static void SetupAndroidToolsFromCmd()
         {
             const string androidJdkPathArgumentName = "overrideAndroidJdkPath";
             const string androidSdkPathArgumentName = "overrideAndroidSdkPath";
             const string androidNdkPathArgumentName = "overrideAndroidNdkPath";
             const string androidGradlePathArgumentName = "overrideAndroidGradlePath";
 
-            if (string.IsNullOrEmpty(AndroidExternalToolsSettings.jdkRootPath) &&
-                Utilities.TryGetCommandLineArgumentValue(androidJdkPathArgumentName, out var androidJdkPath))
+            if (Utilities.TryGetCommandLineArgumentValue(androidJdkPathArgumentName, out var androidJdkPath))
             {
                 AndroidExternalToolsSettings.jdkRootPath = androidJdkPath;
             }
 
-            if (string.IsNullOrEmpty(AndroidExternalToolsSettings.sdkRootPath) &&
-                Utilities.TryGetCommandLineArgumentValue(androidSdkPathArgumentName, out var androidSdkPath))
+            if (Utilities.TryGetCommandLineArgumentValue(androidSdkPathArgumentName, out var androidSdkPath))
             {
                 AndroidExternalToolsSettings.sdkRootPath = androidSdkPath;
             }
 
-            if (string.IsNullOrEmpty(AndroidExternalToolsSettings.ndkRootPath) &&
-                Utilities.TryGetCommandLineArgumentValue(androidNdkPathArgumentName, out var androidNdkPath))
+            if (Utilities.TryGetCommandLineArgumentValue(androidNdkPathArgumentName, out var androidNdkPath))
             {
                 AndroidExternalToolsSettings.ndkRootPath = androidNdkPath;
             }
 
-            if (string.IsNullOrEmpty(AndroidExternalToolsSettings.gradlePath) &&
-                Utilities.TryGetCommandLineArgumentValue(androidGradlePathArgumentName, out var androidGradlePath))
+            if (Utilities.TryGetCommandLineArgumentValue(androidGradlePathArgumentName, out var androidGradlePath))
             {
                 AndroidExternalToolsSettings.gradlePath = androidGradlePath;
             }
